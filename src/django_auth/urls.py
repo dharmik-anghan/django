7f2cff7e112a5cdc882d842b4cc6f109e26ae510
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-01-10 15:52:29 +0530

    CustomUser

--- src/django_auth/urls.py
+++ src/django_auth/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ecommerce project.
+URL configuration for django_auth project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
