8c8e39c9087a2303ea33398b26ff6fb51e731f36
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-01-29 14:40:27 +0530

    Update

--- src/django_auth/urls.py
+++ src/django_auth/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("account.urls")),
-]
+    path("api/post/", include("post.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/post/views.py
+++ src/post/views.py
@@ -1,11 +1,12 @@
 from rest_framework.views import APIView
 from account.renderers import UserRenderer
 from rest_framework.permissions import IsAuthenticated
-from post.serializers import PostSerializer,PostCreateSerializer, PostUpdateSerializer
+from post.serializers import PostSerializer, PostCreateSerializer, PostUpdateSerializer
 from post.models import Post
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from datetime import datetime, timezone
+
 # Create your views here.
 
 
@@ -24,46 +25,38 @@ class GetPostView(APIView):
             except Post.DoesNotExist:
                 return Response({"msg": "Post not found"}, status=404)
 
-
-            serializer = PostSerializer(post, context={'request':request})
+            serializer = PostSerializer(post, context={"request": request})
             return Response({"post": serializer.data})
-        
+
     def post(self, request):
         data = request.data
-        data['user_id'] = request.user.id
+        data["user_id"] = request.user.id
 
-        serializer = PostCreateSerializer(data=data, context={'request':request})
+        serializer = PostCreateSerializer(data=data, context={"request": request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         else:
             return Response(serializer.errors, status=400)
-        
 
     def patch(self, request):
         data = request.data
-        post_id = request.query_params.get('id')
+        post_id = request.query_params.get("id")
         post = Post.objects.get(pk=post_id)
         post.updated_at = datetime.now(timezone.utc)
         post.save()
-        serializer = PostUpdateSerializer(post, data=data, partial=True, context={'request':request})
-        
+        serializer = PostUpdateSerializer(
+            post, data=data, partial=True, context={"request": request}
+        )
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         else:
             return Response(serializer.errors, status=400)
-        
+
     def delete(self, request):
-        post_id = request.query_params.get('id')
+        post_id = request.query_params.get("id")
         post = Post.objects.get(pk=post_id)
         post.delete()
         return Response({"msg": "Post Deleted"}, status=204)
-
-class GetImageFile(APIView):
-    def get(self, request, username, filename):
-        html = """
-            <img src="/media/uploads/admin/1706184713.jpg">
-"""
-
-
