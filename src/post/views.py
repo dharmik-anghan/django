d0df25206ffead34fe267947454fdbf9f09cfa75
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-01-31 11:14:05 +0530

    Feed View Added

--- src/post/views.py
+++ src/post/views.py
@@ -1,11 +1,13 @@
 from rest_framework.views import APIView
 from account.renderers import UserRenderer
 from rest_framework.permissions import IsAuthenticated
-from post.serializers import PostSerializer, PostCreateSerializer, PostUpdateSerializer
+from follow.models import Follow
+from post.serializers import GetFeedSerializer, PostSerializer, PostCreateSerializer, PostUpdateSerializer
 from post.models import Post
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from datetime import datetime, timezone
+from account.models import User
 
 # Create your views here.
 
@@ -15,6 +17,12 @@ class GetPostView(APIView):
     permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
     parser_classes = [MultiPartParser, FormParser, JSONParser]
+
+    def update_post_count(self, user_id):
+        post_count = Post.objects.filter(user_id=user_id).count()
+        user = User.objects.get(id=user_id)
+        user.post_count = post_count
+        user.save()
 
     def get(self, request):
         post_id = request.query_params.get("id")
@@ -35,6 +43,7 @@ class GetPostView(APIView):
         serializer = PostCreateSerializer(data=data, context={"request": request})
         if serializer.is_valid():
             serializer.save()
+            self.update_post_count(request.user.id)
             return Response(serializer.data, status=201)
         else:
             return Response(serializer.errors, status=400)
@@ -42,7 +51,10 @@ class GetPostView(APIView):
     def patch(self, request):
         data = request.data
         post_id = request.query_params.get("id")
-        post = Post.objects.get(pk=post_id)
+        try:
+            post = Post.objects.get(pk=post_id)
+        except Post.DoesNotExist:
+            return Response({"msg": "Post not found"}, status=404)
         post.updated_at = datetime.now(timezone.utc)
         post.save()
         serializer = PostUpdateSerializer(
@@ -57,6 +69,25 @@ class GetPostView(APIView):
 
     def delete(self, request):
         post_id = request.query_params.get("id")
-        post = Post.objects.get(pk=post_id)
+        try:
+            post = Post.objects.get(pk=post_id)
+        except Post.DoesNotExist:
+            return Response({"msg": "Post not found"}, status=404)
         post.delete()
+        self.update_post_count(request.user.id)
         return Response({"msg": "Post Deleted"}, status=204)
+
+
+class GetPostFeedView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
+
+    def get(self, request):
+        user_id = request.user.id
+        users = Follow.objects.filter(followed_by=user_id).all()
+        # posts=Post.objects.filter(user_id=users).all().order_by('created_at').values()
+        print(users)
+        serializer = GetFeedSerializer(users, many=True, context={"request": request})
+        return Response(serializer.data)
