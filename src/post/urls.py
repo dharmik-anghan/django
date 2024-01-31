a3dd3615f2746222f0ddc3735e8dcefba13886ae
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-01-31 11:15:27 +0530

    Feed URLs

--- src/post/urls.py
+++ src/post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from post.views import GetPostView
+from post.views import GetPostView, GetPostFeedView
 
 urlpatterns = [
     path("", GetPostView.as_view()),
+    path("feed", GetPostFeedView.as_view()),
 ]
 
