9889da5998749391d0132498af2fa356804f59db
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-01-31 11:13:09 +0530

    UpdateSerializer

--- src/post/serializers.py
+++ src/post/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from follow.models import Follow
 from post.models import Post
 
 POST_STATUS_CHOICES = (
@@ -9,37 +10,48 @@ POST_STATUS_CHOICES = (
 
 class PostSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
         fields = "__all__"
 
     def get_image(self, obj):
-        request = self.context.get('request')
+        request = self.context.get("request")
+        image = obj.image.url
+        return request.build_absolute_uri(image)
+        return image
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ["id", "image", "description", "user_id", "status"]
+
+
+class PostUpdateSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ["id", "image", "description", "user_id", "status"]
+
+    def get_image(self, obj):
+        request = self.context.get("request")
         image = obj.image.url
         print(image)
         return request.build_absolute_uri(image)
     
+class GetFeedSerializer(serializers.ModelSerializer):
+    def getPost(self, obj):
+        posts = Post.objects.filter(user_id=obj.followed_to.id).all()
+        serializers = PostSerializer(posts, many=True,context=self.context)
+        return serializers.data
+        
+    posts = serializers.SerializerMethodField("getPost")
 
-class PostCreateSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
     class Meta:
-        model = Post
-        fields = ['id','image', 'description', 'user_id', 'status']
-    def get_image(self, obj):
-            request = self.context.get('request')
-            image = obj.image.url
-            print(image)
-            return request.build_absolute_uri(image)
-
-class PostUpdateSerializer(serializers.ModelSerializer):
-    # image = serializers.ImageField(read_only=True)
-    image = serializers.SerializerMethodField(read_only=True)
-    class Meta:
-        model = Post
-        fields = ['id','image','description', 'user_id', 'status']
-
-    def get_image(self, obj):
-            request = self.context.get('request')
-            image = obj.image.url
-            print(image)
-            return request.build_absolute_uri(image)
+        model = Follow
+        fields = (
+            "posts",
+        )
+        read_only_fields = ("id",)
