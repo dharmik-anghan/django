b8b93e4fa7fa53809e196bd7d7eea479012436f7
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-02-08 10:56:35 +0530

    [Dharmik] urls.py

--- src/follow/urls.py
+++ src/follow/urls.py
@@ -3,6 +3,6 @@ from .views import FollowViews, GetFollowerView, GetFollowingView
 
 urlpatterns = [
     path("", FollowViews.as_view()),
-    path("followers", GetFollowerView.as_view()),
-    path("following", GetFollowingView.as_view()),
+    path("followers/", GetFollowerView.as_view()),
+    path("following/", GetFollowingView.as_view()),
 ]
