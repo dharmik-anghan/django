aec889b1239252b698c0f327ee390086331ddd00
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-02-13 16:17:01 +0530

    changes didn't worked

--- src/instag/asgi.py
+++ src/instag/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for instag project.
+ASGI config for django_auth project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_auth.settings")
 
 application = get_asgi_application()
