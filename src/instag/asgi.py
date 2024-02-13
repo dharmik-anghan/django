0300fd39de64a51fe673c5d0bb23e469a876a09b
Dharmik Anghan <dharmikanghan09@gmail.com>
2024-02-13 15:58:39 +0530

    Changed name of project

--- src/instag/asgi.py
+++ src/instag/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_auth project.
+ASGI config for instag project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_auth.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings")
 
 application = get_asgi_application()
--- src/manage.py
+++ src/manage.py
@@ -8,7 +8,7 @@ import dotenv
 def main():
     dotenv.load_dotenv()
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_auth.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "instag.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
